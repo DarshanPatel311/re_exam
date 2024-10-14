540c189e8e40f36f4319f3090d5a2cd1ce4612d9
Darshan Patel <143177575+DarshanPatel311@users.noreply.github.com>
2024-10-14 22:06:38 +0530

    Update login.dart

--- lib/Controoler/login.dart
+++ lib/Controoler/login.dart
@@ -21,35 +21,7 @@ class authController extends GetxController{
     }
   }
 
-  bool validateEmail(String email) {
-    if (email.isEmpty) {
-      emailError.value = 'Email is required';
-      return false;
-    } else if (!email.contains('@gmail.com')) {
-      emailError.value = 'Email must be a valid @gmail.com address';
-      return false;
-    } else {
-      emailError.value = '';
-      return true;
-    }
-  }
-  bool validatePassword(String password) {
-    if (password.isEmpty) {
-      passwordError.value = 'Password is required';
-      return false;
-    } else if (password.length < 8) {
-      passwordError.value = 'Password must be at least 8 characters';
-      return false;
-    } else {
-      passwordError.value = '';
-      return true;
-    }
-  }
-  bool validateForm() {
-    final isEmailValid = validateEmail(txtEmail.text);
-    final isPasswordValid = validatePassword(txtName.text);
-    return isEmailValid && isPasswordValid;
-  }
+  
 
 
 
